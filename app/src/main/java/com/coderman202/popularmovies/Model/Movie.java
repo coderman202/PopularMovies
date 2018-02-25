533ac28b471702a2e9b622b48eb38d96e4725efd
coderman202 <reglyons@gmail.com>
2018-02-25 13:29:54 +0000

    Initial commit

---
+++ app/src/main/java/com/coderman202/popularmovies/Model/Movie.java
@@ -0,0 +1,17 @@
+package com.coderman202.popularmovies.Model;
+
+/**
+ * Created by Reggie on 25/02/2018.
+ * A class to represent movies.
+ */
+
+public class Movie {
+
+    private String title;
+    private String imageLink;
+    private String plotSynopsis;
+    private int userRating;
+    private String releaseDate;
+
+    
+}
