95281005ad3d99f1fd03f6b21d16643b5167f0b1
coderman202 <reglyons@gmail.com>
2018-05-13 02:19:55 +0100

    Version ready for submission.

--- app/src/main/java/com/coderman202/popularmovies/responses/MovieTrailerJsonResponse.java
+++ app/src/main/java/com/coderman202/popularmovies/responses/MovieTrailerJsonResponse.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 /**
  * Created by Reggie on 21/03/2018.
- * Custom class which represents a list of movie trailer objects returned by the API call
+ * Custom class which represents a list of movie trailer objects returned by the API call.
  */
 
 public class MovieTrailerJsonResponse {
